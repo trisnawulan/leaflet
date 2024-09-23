13d8f9f256365058b04d4093926a760a486eff4c
trisnawulan <trisnadiahayuwulandari@mail.ugm.ac.id>
2024-09-23 10:03:33 +0700

    integrasi leaflet pada ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map! : L.Map;
   constructor() {}
+  // ngOnInit() {
+  //   this.map = L.map('map').setView([35.76943, -580081], 13);
+  //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  //   }).addTo(this.map);
+  // }
 
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([35.76943, -580081], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
 }
