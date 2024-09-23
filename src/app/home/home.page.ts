99444b60e8e16aeeb43e82813b35321d11acf041
trisnawulan <trisnadiahayuwulandari@mail.ugm.ac.id>
2024-09-23 10:21:48 +0700

    Penambahan marker & control basemap

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,17 +10,47 @@ export class HomePage {
 
   map! : L.Map;
   constructor() {}
-  // ngOnInit() {
-  //   this.map = L.map('map').setView([35.76943, -580081], 13);
-  //   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //   }).addTo(this.map);
-  // }
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView([35.76943, -580081], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    this.map = L.map('mapId').setView([-7.797068, 110.370529], 13); // Set peta di Yogyakarta
+
+    // OSM Base map
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Satellite Base map
+    const satellite = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Terrain Base map
+    const terrain = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, SRTM | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (CC-BY-SA)'
+    });
+
+    osm.addTo(this.map); // Default basemap
+
+    // Menambahkan layer control untuk memilih basemap
+    L.control.layers({
+      "OpenStreetMap": osm,
+      "Satellite": satellite,
+      "Terrain": terrain
     }).addTo(this.map);
+
+    // Menambahkan beberapa marker museum di Yogyakarta
+    const markers: { coords: [number, number], popup: string }[] = [
+      { coords: [-7.8056, 110.3649], popup: "<b>Museum Keraton Yogyakarta</b><br>Keraton Kesultanan Yogyakarta." },
+      { coords: [-7.8017, 110.3642], popup: "<b>Museum Sonobudoyo</b><br>Museum sejarah dan budaya." },
+      { coords: [-7.7939, 110.3695], popup: "<b>Museum Batik Yogyakarta</b><br>Museum khusus untuk seni batik." },
+      { coords: [-7.7837, 110.3893], popup: "<b>Museum Affandi</b><br>Museum seni pelukis terkenal, Affandi." },
+      { coords: [-7.8226, 110.3939], popup: "<b>Museum De Mata Trick Eye</b><br>Museum seni 3D interaktif." }
+    ];
+
+    // Loop untuk menambahkan beberapa marker ke peta
+    markers.forEach(markerData => {
+      const marker = L.marker(markerData.coords).addTo(this.map); // Tidak perlu konversi
+      marker.bindPopup(markerData.popup);
+    });
   }
 }
